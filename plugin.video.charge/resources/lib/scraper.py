855b36f8b363586a9d5d53512a6e194c0b6db426
Allen Lew <alew@magnite.com>
2024-11-19 11:10:05 -0800

    fix: fix regex for charge

--- plugin.video.charge/resources/lib/scraper.py
+++ plugin.video.charge/resources/lib/scraper.py
@@ -9,6 +9,6 @@ class myAddon(t1mAddon):
 
   def getAddonMenu(self,url,ilist):
       html = requests.get('https://watchcharge.com/watch-live/', headers=self.defaultHeaders).text
-      url = re.compile('file: "(.+?)"', re.DOTALL).search(html).group(1)
+      url = re.compile('\'file\' : "(.+?)"', re.DOTALL).search(html).group(1)
       url = ''.join([url,'|20534|35794%7C23.2'])
       return self.getAddonListing(url, ilist)
